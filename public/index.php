b4dc205bc855d9046aefc762368ed2a7213181c5
kriss <krissant27@gmail.com>
2022-11-03 11:48:36 +0200

    tira vide

--- public/index.php
+++ public/index.php
@@ -2,26 +2,9 @@
 
 declare(strict_types=1);
 
-require_once '../src/Car.php';
 
-// $obj = stdClass();
+spl_autoload_register(function($class){
+    require_once  __DIR__ . '/../' . lcfirst(str_replace('\\', '', $class));
+});
 
-
-
-# homework: driver (name, surname, age)
-//xhprof_disable()
-$car =(new Car(1000, number_plate: 'AB-1234', fuelCosumption: 14.6))
-    ->addKilometers(kilometers: 200)
-    ->addKilometers(kilometers: 500)
-    ->getConsumedLiters();
-
-$car =(new Car(odometer: 12500, number_plate: 'ZZ-23', fuelCosumption: 4.6))
-    ->addKilometers(kilometers: 200)
-    ->addKilometers(kilometers: 500)
-    ->getConsumedLiters();
-
-$Driver =(new Driver(name: 'jankels', surname: 'beerzs', age: 54));
-
-
-
-//require __DIR__ . '/../bootstrap/app.php';
+$transactions = new \App\ServiceCosts();
