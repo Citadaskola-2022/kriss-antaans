b906779ff76891f6963eba2a044770c0e25909ea
kriss <krissant27@gmail.com>
2022-11-03 15:21:07 +0200

    3rd party packages, env variables

--- public/index.php
+++ public/index.php
@@ -4,7 +4,43 @@ declare(strict_types=1);
 
 use App\ServiceCosts;
 
+use Doctrine\Inflector\InflectorFactory;
+use Ramsey\Uuid\Uuid;
+
 require __DIR__ . '/../bootstrap/app.php';
 
 $transactions = new ServiceCosts(200);
-$transactions->process();
+
+//$transactions->process();
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+
+//echo '<pre>';
+var_dump($_ENV['SECRET_KEY']);
+
+$inflector = InflectorFactory::create()->build();
+
+$name = 'apple';
+printf(
+    'vienskaitlis: %s, daudzskaitlis: %s',
+    $name, $inflector->pluralize($name)
+);
+echo '<br>';
+
+
+$blogPost = 'This is MY 3yd4gy Blog post';
+printf(
+    'title: %s, url: %s',
+    $blogPost, $inflector->urlize($blogPost)
+);
+echo '<br>';
+
+$uuid = Uuid::uuid4();
+
+printf(
+    "UUID: %s\nVersion: %d\n",
+    $uuid->toString(),
+    $uuid->getFields()->getVersion()
+);
