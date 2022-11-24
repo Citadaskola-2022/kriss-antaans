bdc15dedd88e03daa03446c26e01f1eb09c4119b
kriss <krissant27@gmail.com>
2022-11-24 13:35:26 +0200

    mantosana, abstraktas klases un metodes

--- public/index.php
+++ public/index.php
@@ -2,30 +2,14 @@
 
 declare(strict_types=1);
 require __DIR__ . '/../bootstrap/app.php';
-$coffee = new \App\Cafe\CoffeeWithMilk();
-
-var_dump($coffee);
-
-$coffee->prepare(50);
-$coffee->prepare(40);
-$coffee->prepare(30);
 
 
-makeCoffee($coffee);
+$fields = [
+    new \App\Form\Text('text_field'),
+    new \App\Form\Checkbox('checkbox_field'),
+    new \App\Form\Radio('radio_field'),
+];
 
-$coffee->brew();
-
-function makeCoffee(\App\Cafe\Coffee $coffee): void
-{
-   // if($coffee instanceof \App\Cafe\CoffeeWithMilk)
-    $coffee->addMilk();
+foreach ($fields as $field){
+    echo $field->render();
 }
-
-$coffee = new \App\Cafe\CoffeeWithMilk();
-$iced = new \App\Cafe\IcedCoffee($coffee);
-$iced->prepare(50);
-$iced->addIce(5);
-$iced->addIce(50);
-$iced->brew();
-
-var_dump($iced);
